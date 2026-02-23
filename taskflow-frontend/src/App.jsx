49830733607f4892d70283e4c5cf1d19179bd94a
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-02-23 13:31:47 +0530

    Updated my app

--- taskflow-frontend/src/App.jsx
+++ taskflow-frontend/src/App.jsx
@@ -2,13 +2,32 @@ import { Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import ProjectDetails from './pages/ProjectDetails';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Dashboard />} />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/projects/:projectId"
+        element={
+          <ProtectedRoute>
+            <ProjectDetails />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
