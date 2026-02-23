d22238cc3a2f65f46afd795d5d75f65481ff62fa
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-02-23 13:09:17 +0530

    update dashboard design

--- taskflow-frontend/src/pages/Dashboard.jsx
+++ taskflow-frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from 'react';
+import api from '../services/api';
+import toast from 'react-hot-toast';
+
 function Dashboard() {
-  return <h2>Dashboard</h2>;
+  const [projects, setProjects] = useState([]);
+  const [name, setName] = useState('');
+
+  const fetchProjects = async () => {
+    try {
+      const res = await api.get('/projects');
+      setProjects(res.data);
+    } catch (error) {
+      toast.error('Failed to fetch projects');
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const handleCreate = async (e) => {
+    e.preventDefault();
+
+    try {
+      await api.post('/projects', { name });
+      setName('');
+      fetchProjects();
+      toast.success('Project created');
+    } catch (error) {
+      toast.error('Error creating project');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+
+      <form onSubmit={handleCreate}>
+        <input
+          placeholder="Project Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Create Project</button>
+      </form>
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project._id}>{project.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Dashboard;
